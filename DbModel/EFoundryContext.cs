f596b797d49367e1c62aa5f150864b994f195551
March <supper0508@gmail.com>
2016-07-14 11:02:46 +0800

    123

---
+++ DbModel/EFoundryContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity.Validation;
+
+namespace DbModel
+{
+    public class EFoundryContext:DbEntities
+    {
+        public override int SaveChanges()
+        {
+            try
+            {
+                base.Configuration.ValidateOnSaveEnabled = false;
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                base.Configuration.ValidateOnSaveEnabled = true;
+            }
+            
+        }
+    }
+}
---
+++ DbModel/ViewModel/Login/LoginVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbModel.ViewModel.Login
+{
+    public class LoginVM
+    {
+        [DisplayName("帳號"), Required, RegularExpression("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}")]
+        public string UserEmail { get; set; }
+
+        [DisplayName("密碼"), Required]
+        public string Password { get; set; }
+    }
+}
